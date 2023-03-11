f789a97432b032ac35d775b1b91774231e9158db
TiewY <yingyingtiew@gmail.com>
2023-03-11 15:14:15 +0000

    Final push

--- public/src/App.js
+++ public/src/App.js
@@ -3,12 +3,12 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import UpdateProfileforUser from "./pages/UpdateProfileforUser";
 import LeadersDirectory from "./pages/LeadersDirectory";
-import UpdateProfileforTechLead from "./pages/UpdateProfileforTechLead";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
 export default function App(){
   return (
+    <>
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<Home />} />
@@ -18,5 +18,6 @@ export default function App(){
     <Route path="/updateTech" element={<UpdateProfileforTechLead />} />
   </Routes>
   </BrowserRouter>
+  </>
   );
 }
