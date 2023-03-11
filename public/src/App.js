0470598caadaeb259629f500b93785c4b76d61bb
TiewY <yingyingtiew@gmail.com>
2023-03-11 12:42:38 +0000

    Final

--- public/src/App.js
+++ public/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import UpdateProfileforUser from "./pages/UpdateProfileforUser";
+import LeadersDirectory from "./pages/LeadersDirectory";
 import UpdateProfileforTechLead from "./pages/UpdateProfileforTechLead";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -11,7 +12,9 @@ export default function App(){
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
     <Route path="/update" element={<UpdateProfileforUser />} />
+    <Route path="/directory" element={<LeadersDirectory />} />
     <Route path="/updateTech" element={<UpdateProfileforTechLead />} />
   </Routes>
   </BrowserRouter>
