f0976667c181fe982dfb47dab0a322b3bfdb0780
Pawandi-W <pawandiwije@gmail.com>
2023-03-11 16:39:04 +0000

    Login and Register UI

--- public/src/App.js
+++ public/src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import UpdateProfileforUser from "./pages/UpdateProfileforUser";
+import UpdateProfileforTechLead from "./pages/UpdateProfileforTechLead";
 import LeadersDirectory from "./pages/LeadersDirectory";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
@@ -12,6 +15,8 @@ export default function App(){
   <BrowserRouter>
   <Routes>
     <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
     <Route path="/update" element={<UpdateProfileforUser />} />
     <Route path="/directory" element={<LeadersDirectory />} />
     <Route path="/updateTech" element={<UpdateProfileforTechLead />} />
---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+import {Link } from "react-router-dom";
+import NavBar from "../components/Pawandi/Navbar";
+import Footer from "../components/Pawandi/Footer";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-color: #0C3640;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px 0 10px;
+`;
+/*
+const Logo = styled.img`
+  height: 5rem;
+  margin-right: 2rem;
+`;
+*/
+const HeaderText = styled.h1`
+  
+  color: #8BD9D9;
+  
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  padding: 20px;
+  background-color: #8BD9D9;
+  border-radius: 20px;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 5px;
+  width: 100%;
+`;
+
+const Label = styled.label`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #0c3640;
+  margin-bottom: 6px;
+`;
+
+const Input = styled.input`
+  font-size: 1.2rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  border: none;
+  background-color: #60bfbf;
+  color: #0c3640;
+`;
+
+const SubmitButton = styled.button`
+  font-size: 1.2rem;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  border: none;
+  background-color: #2b838c;
+  color: #ffffff;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #185359;
+  }
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+`;
+
+const LinkContainer = styled.div`
+  margin-top: 2rem;
+  font-size: 1.2rem;
+  color: #0c3640;
+`;
+
+const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission logic
+  };
+  return(
+<>
+<NavBar/>
+<Container>
+<p></p> 
+      <HeaderContainer>
+        
+        <HeaderText>Register</HeaderText>
+      </HeaderContainer>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="first-name">First Name</Label>
+          <Input
+            type="text"
+            id="first-name"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="last-name">Last Name</Label>
+          <Input
+            type="text"
+            id="          last-name"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="confirm-password">Confirm Password</Label>
+          <Input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            required
+          />
+        </FormGroup>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <SubmitButton type="submit">Register</SubmitButton>
+      </Form>
+      <LinkContainer>
+        <Link to="/login">Already Registerd?</Link>
+      </LinkContainer>
+    </Container>
+<Footer/>
+    </>
+ );
+};
+
+export default Register;
