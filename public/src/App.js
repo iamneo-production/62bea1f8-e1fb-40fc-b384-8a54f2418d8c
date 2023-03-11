2c49fc5a949cc61a6b656d7e85d99573ef15a2fd
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-03-11 10:03:52 +0530

    change

--- public/src/App.js
+++ public/src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import Profile from "./pages/Profile";
-=======
 import UpdateProfileforUser from "./pages/UpdateProfileforUser";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
->>>>>>> 40ff0ddbcb90a3bbae98a93d3c09d60de2296e42
 
-export default function App(){
+export default function App() {
   return (
     <>
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/update" element={<UpdateProfileforUser />} />
-    <Route path="/profile" element={<Profile />} />
-  </Routes>
-  </BrowserRouter>
-  </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/update" element={<UpdateProfileforUser />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
