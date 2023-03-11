ca1f607151e33e940155eb2e8d1c6239c1712314
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-03-11 15:42:36 +0000

    final changes

--- public/src/App.js
+++ public/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
 import UpdateProfileforUser from "./pages/UpdateProfileforUser";
+import LeadersDirectory from "./pages/LeadersDirectory";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-export default function App() {
+export default function App(){
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/update" element={<UpdateProfileforUser />} />
-          <Route path="/profile/:user/:techleadId" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route path="/update" element={<UpdateProfileforUser />} />
+    <Route path="/directory" element={<LeadersDirectory />} />
+    <Route path="/updateTech" element={<UpdateProfileforTechLead />} />
+  </Routes>
+  </BrowserRouter>
+  </>
   );
 }
--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -3,14 +3,15 @@ import NavBar from "../components/Pawandi/Navbar";
 import Footer from "../components/Pawandi/Footer";
 import Slider from "../components/Pawandi/Slider";
 
-function Home() {
-  return (
-    <>
-      <NavBar />
-      <Slider />
-      <Footer />
+
+function Home(){
+    return(
+<>
+<NavBar/>
+<Slider/>
+<Footer/>
     </>
-  );
+    );
 }
 
 export default Home;
