0dc0da7c02d828aebd80daa8734e23a96950626b
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-03-11 13:31:43 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const profile = require("./profile");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,28 +6,35 @@ require("dotenv").config();
 
 const mysql = require("mysql");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'examly',
-    database: 'herimpact',
-  });
-  pool.getConnection((error, connection) => {
-    if (error) {
-      console.error("Error connecting to database:", error);
-      return;
-    }
-    console.log("Connected to MySQL database!");
-    connection.release();
-  });
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "examly",
+//   database: "herimpact",
+// });
+// pool.getConnection((error, connection) => {
+//   if (error) {
+//     console.error("Error connecting to database:", error);
+//     return;
+//   }
+//   console.log("Connected to MySQL database!");
+//   connection.release();
+// });
 
 app.use(cors());
 app.use(express.json());
+app.use("/profile", profile);
 const port = 8080;
-
-const server = app.listen(port,()=>{
-    console.log("Server Started on Port 8080")
+app.get("/techlead", (req, res) => {
+  const sqlGet = "select * from techlead";
+  pool.query(sqlGet, (err, result) => {
+    if (err) res.send({ err: err });
+    else res.send(result);
+  });
 });
 
+const server = app.listen(port, () => {
+  console.log("Server Started on Port 8080");
+});
 
-module.exports = pool;
+// module.exports = pool;
